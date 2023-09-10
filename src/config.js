b0cfc406dcec79ebb0e8915e19aee8478ba75253
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-09-10 13:54:34 +0500

    add

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const PORT = 9000;
+const PORT = 6000;
 connection = {
   connectionStringEL: "postgres://iwfoovqs:CjDd1vtZWDGLp5MZs-eBEiWCUppldQEe@manny.db.elephantsql.com/iwfoovqs",
   // connectionStringEL: "postgres://jvesrinh:nH4LbnttMxxtujfDFolGSMp2pTt1Ro7Z@tyke.db.elephantsql.com/jvesrinh",
