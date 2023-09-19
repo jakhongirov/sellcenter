ea1abb50567e5eed82c8eac7d0a7ec362ee09718
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-09-19 11:37:10 +0500

    add

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -3,12 +3,15 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.resolve(__dirname, '..','..','public/images'));
+        cb(null, path.resolve(__dirname, '..', '..', 'public/images'));
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() +'-'+ file.originalname.split(" ").join(""))
+        cb(null, Date.now() + '-' + file.originalname.split(" ").join(""))
     }
 })
-const upload = multer({ storage: storage })
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 3 * 1024 * 1024 } // Set maximum file size to 3 MB
+})
 
 module.exports = upload
